4d5734761f1307cdb20fb8aa3171b29e032e011f
Ashish Padiyar <ashishpadiyar@gmail.com>
2026-06-19 01:41:11 -0400

    completed project

---
+++ server.py
@@ -0,0 +1,32 @@
+''' server.py for web deployment of emotion detector'''
+from flask import Flask,render_template,request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def emotion_detect():
+    '''funtion to detect emotion of text'''
+    text_to_analyze = request.args.get("textToAnalyze")
+    response = emotion_detector(text_to_analyze)
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!."
+    res = (
+        f"For the given statement, the system response is "
+        f"'anger': {response['anger']}, "
+        f"'disgust': {response['disgust']}, "
+        f"'fear': {response['fear']}, "
+        f"'joy': {response['joy']} and "
+        f"'sadness': {response['sadness']}. "
+        f"The dominant emotion is "
+        f"{response['dominant_emotion']}."
+    )
+    return res
+
+@app.route("/")
+def render_index_page():
+    ''' render index.html'''
+    return render_template('index.html')
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0",port=5000)
